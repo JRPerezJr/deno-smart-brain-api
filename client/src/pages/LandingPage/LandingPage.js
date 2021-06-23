95019a351d6f1c128ece434f5d8afb873ed73d86
Juan <pront23@gmail.com>
2021-06-23 23:28:30 +0900

    renamed folder to pages

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'shards-react';
+
+export default function LandingPage() {
+  return (
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+  );
+}
